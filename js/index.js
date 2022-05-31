8126785e03a7b117655d5ca957862a97316cd029
张已涛 <1151259301@qq.com>
2022-05-31 14:35:02 +0800

    '第一次提交框架'

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// 点击我的跳转到我的登录页
+
+var header2 = document.getElementsByClassName('header2')[0];
+console.log(header2);
+
+header2.onclick = function () {
+    window.location.href = './wodeyemian.html';
+}
